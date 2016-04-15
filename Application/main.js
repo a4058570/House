d11c1965d48d59dc1c99d0551c7d6eeb8cafaa33
wangshiwen <wagnshiwen@justmeyo.com>
2016-04-15 10:43:51 +0800

    111111111111

--- Application/main.js
+++ Application/main.js
@@ -29,3 +29,13 @@ defineClass("BussVC", {
             console.log("调用jspatch成功")
             },
             })
+
+// JS
+defineClass("BussVC", {
+            //实例方法
+            
+            //类方法
+            jsPatchText: function() {
+            console.log("调用jspatch成功")
+            },
+            })
